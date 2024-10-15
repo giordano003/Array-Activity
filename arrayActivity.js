a39d2442b0fe0799a5df92e30d4baad0e0b0cc74
Jordan <ligadosjordan3@gmail.com>
2024-10-15 20:16:19 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+// Create an array called favSingers and store your three favorite singers in it.
+let favSingers = ["Luke Combs", "Brooks and Dunn", "George Strait"];
+
+// Log the first singer in the array using array index notation.
+console.log(favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers.
+let favNumbers = [13, 6, 27, 30];
+
+// Create an array named mixedArr and store different data types: ["string", ["otherarray"], 123, true].
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in the mixedArr using array index notation.
+console.log( mixedArr[0]);
+console.log( mixedArr[1]);
+console.log( mixedArr[2]);
+console.log( mixedArr[3]);
